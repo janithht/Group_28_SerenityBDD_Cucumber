fe98a5378b33257f632af21b0329760dd634109b
Janith Hathnagoda <92964360+janithht@users.noreply.github.com>
2024-11-17 10:11:31 +0530

    First test created

--- src/test/java/AppTests/TestRunner.java
+++ src/test/java/AppTests/TestRunner.java
@@ -1,12 +1,8 @@
 package AppTests;
 
-import Steps.LoginPageSteps;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.CucumberSerenityRunner;
-import net.serenitybdd.annotations.Managed;
-import net.serenitybdd.annotations.Steps;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
 
 @RunWith(CucumberSerenityRunner.class)
 @CucumberOptions(features = "src/test/resources/features",glue = "Steps",plugin = {
