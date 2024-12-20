248ac06876027f616617f8e3d27de95598d5ce37
Devin-Bandara <122140695+Devin-Bandara@users.noreply.github.com>
2024-12-20 12:58:56 +0530

    Isolate API and UI tests in Cucumber runner

--- src/test/java/AppTests/UITests.java
+++ src/test/java/AppTests/UITests.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = "src/test/resources/features/ui",
-        glue = "Steps",
+        glue = "Steps.UIStepDefinitions",
         plugin = {"pretty"}
 )
 public class UITests {}
