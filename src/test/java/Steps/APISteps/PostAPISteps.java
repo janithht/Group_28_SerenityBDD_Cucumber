c206323ec1fa55e8b1fa25bf24cd45fe8b40f384
Devin-Bandara <122140695+Devin-Bandara@users.noreply.github.com>
2024-12-25 17:27:28 +0530

    added user request function for RequestFactory

--- src/test/java/Steps/APISteps/PostAPISteps.java
+++ src/test/java/Steps/APISteps/PostAPISteps.java
@@ -1,63 +1,47 @@
 package Steps.APISteps;
 
+import Steps.factory.RequestFactory;
 import io.cucumber.java.en.*;
 import io.restassured.response.Response;
-import net.serenitybdd.rest.SerenityRest;
-import static net.serenitybdd.rest.SerenityRest.given;
+import Steps.api.BookAPI;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.is;
 public class PostAPISteps {
 
-    private static final String POST_ENDPOINT = "http://localhost:7081/api/books/";
+    private final BookAPI bookAPI = new BookAPI();
     private Response response;
     private String bookTitle;
     private String bookAuthor;
 
     @Given("User is authorized to create a book as an admin")
     public void user_is_authorized_to_create_a_book_as_an_admin() {
-        SerenityRest.reset();
-        given().auth().preemptive().basic("admin", "password");
+        RequestFactory.setCurrentRequest(RequestFactory.adminRequest());
     }
 
     @Given("User is authorized as a regular user")
     public void user_is_authorized_as_a_regular_user() {
-        SerenityRest.reset();
-        given().auth().preemptive().basic("user", "password");
+        RequestFactory.setCurrentRequest(RequestFactory.userRequest());
     }
 
     @When("User creates a new book with title {string} and author {string}")
     public void user_creates_a_new_book_with_title_and_author(String title, String author) {
         this.bookTitle = title;
         this.bookAuthor = author;
-
-        String requestBody = String.format("{\"title\": \"%s\", \"author\": \"%s\"}", title, author);
-
-        response = given()
-                .auth().preemptive().basic("admin", "password") // Admin authorization
-                .contentType("application/json")
-                .body(requestBody)
-                .when().post(POST_ENDPOINT);
+        response = bookAPI.createBook(title, author);
     }
 
     @When("User submits a POST request to create a book with title {string} and author {string}")
     public void user_submits_a_post_request_to_create_a_book_with_title_and_author(String title, String author) {
         this.bookTitle = title;
         this.bookAuthor = author;
-
-        String requestBody = String.format("{\"title\": \"%s\", \"author\": \"%s\"}", title, author);
-
-        response = given()
-                .auth().preemptive().basic("user", "password") // Regular user authorization
-                .contentType("application/json")
-                .body(requestBody)
-                .when().post(POST_ENDPOINT);
+        response = bookAPI.createBook(title, author);
     }
 
-    @Then("application should return status code 201 for create operation")
-    public void application_should_return_status_code_201_for_create_operation() {
-        assertThat(response.getStatusCode(), is(equalTo(201)));
+    @Then("application should return status code {int} for create operation")
+    public void application_should_return_status_code_for_create_operation(int statusCode) {
+        assertThat(response.getStatusCode(), is(equalTo(statusCode)));
     }
 
     @And("the response should include the book ID")
@@ -74,9 +58,9 @@ public class PostAPISteps {
         assertThat(createdTitle, equalTo(bookTitle));
         assertThat(createdAuthor, equalTo(bookAuthor));
     }
-    @Then("application should return status code 208 for already reported operation")
-    public void application_should_return_status_code_208_for_already_reported_operation() {
-        assertThat(response.getStatusCode(), is(equalTo(208)));
+    @Then("application should return status code {int} for already reported operation")
+    public void application_should_return_status_code_for_already_reported_operation(int statusCode) {
+        assertThat(response.getStatusCode(), is(equalTo(statusCode)));
     }
 
     @And("the response message should be {string}")
@@ -86,17 +70,12 @@ public class PostAPISteps {
     }
 
     @When("User creates a new book without a title and with author {string}")
-    public void user_creates_a_new_book_without_a_title_and_with_author(String author) {
-        String payload = String.format("{\"author\": \"%s\"}", author);
-        response = given()
-                .auth().preemptive().basic("user", "password")
-                .contentType("application/json")
-                .body(payload)
-                .when().post(POST_ENDPOINT);
+    public void user_creates_a_new_book_without_a_title_and_with_author_as_role(String author) {
+        response = bookAPI.createBookWithoutTitle(author);
     }
 
-    @Then("the application should return status code 400 for bad request")
-    public void the_application_should_return_status_code_400() {
-        assertThat(response.getStatusCode(), is(equalTo(400)));
+    @Then("the application should return status code {int} for bad request")
+    public void the_application_should_return_status_code_for_bad_request(int statusCode) {
+        assertThat(response.getStatusCode(), is(equalTo(statusCode)));
     }
 }
